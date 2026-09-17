fe828a55bcbca2b29092090a6782f130c9eca202
Md Kaif <codersb001b002@outlook.com>
2026-09-17 15:30:36 +0530

    Connect frontend to Railway API

--- src/CreateAxios.js
+++ src/CreateAxios.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 export const CreateAxios = axios.create({
-    baseURL: "https://rhomboidally-preformationary-catherina.ngrok-free.dev/api/",
+    baseURL: "https://task-manager-production-dd36.up.railway.app/api/",
     headers: {
         "ngrok-skip-browser-warning": true,
     }
 })
 export const createAxiosWithToken = (token) => axios.create({
-    baseURL: "https://rhomboidally-preformationary-catherina.ngrok-free.dev/api/",
+    baseURL: "https://task-manager-production-dd36.up.railway.app/api/",
     headers: {
         "ngrok-skip-browser-warning": true,
         Authorization: 'Bearer ' + token,
